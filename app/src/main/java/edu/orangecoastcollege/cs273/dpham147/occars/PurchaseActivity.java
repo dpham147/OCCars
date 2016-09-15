2b2d1d442df7b826c39d3b5d47d710050e22791b
dpham147 <dannypham92@hotmail.com>
2016-09-15 15:25:48 -0700

    added intent

--- app/src/main/java/edu/orangecoastcollege/cs273/dpham147/occars/PurchaseActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/dpham147/occars/PurchaseActivity.java
@@ -1,13 +1,74 @@
 package edu.orangecoastcollege.cs273.dpham147.occars;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
-public class PurchaseActivity extends AppCompatActivity {
+public class PurchaseActivity extends Activity {
+
+    private EditText carPriceEditText;
+    private EditText downPaymentEditText;
+    private RadioButton threeYearsRadioButton;
+    private RadioButton fourYearsRadioButton;
+    private RadioButton fiveYearsRadioButton;
+
+    private Car currentCar;
+
+    private String monthlyPaymentText;
+    private String loanSummaryText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_purchase);
+
+        carPriceEditText = (EditText) findViewById(R.id.carPriceEditText);
+        downPaymentEditText = (EditText) findViewById(R.id.downPaymentEditText);
+        threeYearsRadioButton = (RadioButton) findViewById(R.id.threeYearsRadioButton);
+        fourYearsRadioButton = (RadioButton) findViewById(R.id.fourYearsRadioButton);
+        fiveYearsRadioButton = (RadioButton) findViewById(R.id.fiveYearsRadioButton);
+    }
+
+    // Associating button with an event, set the onClick property
+    // Define method as public void with 1 param View view
+    public void activateLoanReport(View view){
+        double price, downPayment;
+
+        try {
+            price = Double.parseDouble(carPriceEditText.getText().toString());
+            downPayment = Double.parseDouble(downPaymentEditText.getText().toString());
+        }
+        catch (NumberFormatException e){
+            price = 0.0;
+            downPayment = 0.0;
+            }
+
+        int loanTerm;
+        if (fiveYearsRadioButton.isChecked())
+            loanTerm = 5;
+        else if (fourYearsRadioButton.isChecked())
+            loanTerm = 4;
+        else
+            loanTerm = 3;
+
+        currentCar.setmPrice(price);
+        currentCar.setmDownPayment(downPayment);
+        currentCar.setmLoanTerm(loanTerm);
+
+        constructLoanSummaryText();
+
+        // Create the intent to share data with LoanSummaryActivity
+        Intent loanSummaryIntent = new Intent(this, LoanSummaryActivity.class);
+        loanSummaryIntent.putExtra("MonthlyPayment", monthlyPaymentText);
+        loanSummaryIntent.putExtra("LoanSummary", loanSummaryText);
+        // Start the new activity
+        startActivity(loanSummaryIntent);
+    }
+
+    private void constructLoanSummaryText(){
+        monthlyPaymentText = getString(R.string.report_line1) +
     }
 }
