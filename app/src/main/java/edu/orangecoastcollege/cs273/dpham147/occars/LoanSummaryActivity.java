8ae840023224bffe66118728b5de130fecc6a1b4
dpham147 <dannypham92@hotmail.com>
2016-09-15 16:56:35 -0700

    activity loan summary xml

--- app/src/main/java/edu/orangecoastcollege/cs273/dpham147/occars/LoanSummaryActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/dpham147/occars/LoanSummaryActivity.java
@@ -1,13 +1,32 @@
 package edu.orangecoastcollege.cs273.dpham147.occars;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-/**
- * Created by Kyubey on 2016-09-15.
- */
-public class LoanSummaryActivity {
-    protected void onCreate{
+public class LoanSummaryActivity extends Activity {
+
+    private TextView monthlyPaymentTextView;
+    private TextView loanSummaryTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loan_summary);
+
+        monthlyPaymentTextView = (TextView) findViewById(R.id.montlyPaymentTextView);
+        loanSummaryTextView = (TextView) findViewById(R.id.loanSummaryTextView);
+
         Intent intentFromPurchaseActivity = getIntent();
+        String monthlyPaymentText = intentFromPurchaseActivity.getStringExtra("MonthlyPayment");
+        monthlyPaymentTextView.setText(monthlyPaymentText);
+        String loanSummaryText = intentFromPurchaseActivity.getStringExtra("LoanSummary");
+        loanSummaryTextView.setText(loanSummaryText);
     }
 
+    public void returnToDataEntry(View view){
+        this.finish();
+    }
 }
