2eda64c606a43c17f772ef1d1e197003c1f262b4
dpham147 <dannypham92@hotmail.com>
2016-09-15 16:31:02 -0700

    activity loan summary xml

--- app/src/main/java/edu/orangecoastcollege/cs273/dpham147/occars/LoanSummaryActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/dpham147/occars/LoanSummaryActivity.java
@@ -1,7 +1,13 @@
 package edu.orangecoastcollege.cs273.dpham147.occars;
 
+import android.content.Intent;
+
 /**
  * Created by Kyubey on 2016-09-15.
  */
 public class LoanSummaryActivity {
+    protected void onCreate{
+        Intent intentFromPurchaseActivity = getIntent();
+    }
+
 }
